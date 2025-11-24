2dd9cebeb922dd9c83c48b6a4b94ae4c6f45acf8
shivsharma-cmyk <shiv.sharma181108@gmail.com>
2025-11-24 09:53:52 -0600

    added function to start to move mario

--- sketch.js
+++ sketch.js
@@ -5,6 +5,12 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let marioImg;
+
+// puting the image in
+function preload() {
+  marioImg = loadImage("mario.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,18 +20,21 @@ function draw() {
   background(220);
 }
 
-class Player {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.speed = 4;
-  }
-
-  display() {
-
-  }
+// move image to x and y
+function movePlayer() {
 }
 
-function preload() {
-  marioImg = loadImage("mario.png");
+function keyPressed() {
+  // move image up
+  if (key === "w") {
+    movePlayer();
+  }
+  // move image left
+  else if (key === "a") {
+    movePlayer();
+  }
+  // move image right
+  else if (key === "d") {
+    movePlayer();
+  }
 }
